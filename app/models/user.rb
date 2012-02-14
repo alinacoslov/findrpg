419766229fcaa0ca811df22e93719ed4dbb66b09
roobeedeedada <bulina123@gmail.com>
2012-02-14 20:42:25 +0000

    Add user validations and testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :username, :email, :password, :password_confirmation, :remember_me
+  
+  # Validations
+  validates :username,              :presence => :true, :uniqueness => true, :length => { :minimum => 3, :maximum => 50 }
+  
 end
