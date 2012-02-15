56e1cc75363846b685ca710134709f90eb076b38
roobeedeedada <bulina123@gmail.com>
2012-02-15 01:04:53 +0000

    Delete redundant tabs

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ActiveRecord::Base
   attr_accessible :username, :email, :password, :password_confirmation, :remember_me
   
   # Validations
-  validates :username,              :presence => :true, :uniqueness => true, :length => { :minimum => 3, :maximum => 50 }
+  validates :username,  :presence => :true, :uniqueness => true, :length => { :minimum => 3, :maximum => 50 }
   
 end
